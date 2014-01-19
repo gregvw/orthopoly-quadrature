a980a87e436ac035855d363904d4b8967e0f93a6
Greg von Winckel <greg.von.winckel@gmail>
2014-01-19 10:08:12 -0700

    Added Clenshaw Curtis quadrature. Update README

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 orthopoly-quadrature
 ====================
 
-Cython adaption of Walter Gautschi's OPQ Matlab suite for generating 
+Orthopoly Quadrature is a collection of Cython scripts for computing
+various types of orthogonal polynomials and quadrature rules. Most of
+the codes are based on Walter Gautschi's OPQ Matlab suite for generating 
 orthogonal polynomials and quadrature rules
 
 https://www.cs.purdue.edu/archives/2002/wxg/codes/OPQ.html
+
---
+++ clencurt.pyx
@@ -0,0 +1,46 @@
+cimport cython
+import numpy as np
+cimport numpy as np
+from scipy.fftpack import ifft
+
+dummy_gobal_name = "foo"
+
+DTYPE = np.float64
+ctypedef np.float64_t DTYPE_t
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def clencurt(int N1):
+    """
+    Compute the Clenshaw Curtis quadrature nodes and weights using the 
+    Fast Fourier Transorm. The nodes are the extrema of the Chebyshev
+    polynomials, -1, and +1.
+    
+    Reference: Jörg Waldvogel, "Fast Construction of the Fejér and
+               Clenshaw-Curtis Quadrature Rules," BIT Numerical Mathematics 
+               46 (1), p. 195-202 (2006).
+
+    http://www.sam.math.ethz.ch/~waldvoge/Papers/fejer.pdf
+    """
+
+    cdef int N = N1-1
+    cdef np.ndarray[DTYPE_t,ndim=1] x
+    cdef np.ndarray[DTYPE_t,ndim=1] w
+    cdef np.ndarray[DTYPE_t,ndim=2] C
+    cdef np.ndarray[DTYPE_t,ndim=1] k
+ 
+    if N1 == 1:
+        x = 0
+        w = 2
+    else:    
+        C = np.zeros((N1,2),DTYPE)
+        k = 2.0*(1.0+np.arange(np.floor(0.5*N)))
+        C[::2,0] = 2.0/np.hstack((1.0, 1.0-k*k))
+        C[1,1] = -N
+        V = np.vstack((C,np.flipud(C[1:N,:])))
+        F = np.real(ifft(V, n=None, axis=0))
+        x = F[0:N1,1]
+        w = np.hstack((F[0,0],2*F[1:N,0],F[N,0]))
+    return x,w   
+ 
+

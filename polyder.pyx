e2390e056786fcc051eea1366848b10e46884979
Greg von Winckel <greg.von.winckel@gmail>
2014-01-19 20:31:11 -0700

    Corrected polyvander, added polyder for computing derivatives of polynomials, updated setup.py

--- polyder.pyx
+++ polyder.pyx
@@ -1,6 +1,8 @@
 cimport cython
 import numpy as np
 cimport numpy as np
+from polyvander import polyvander
+from scipy.misc import factorial
 
 dummy_gobal_name = "foo"
 
@@ -9,13 +11,13 @@ ctypedef np.float64_t DTYPE_t
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-def polyvander(np.ndarray[DTYPE_t,ndim=1] alpha,
+def polyder(np.ndarray[DTYPE_t,ndim=1] alpha,
             np.ndarray[DTYPE_t,ndim=1] beta,
-            np.ndarray[DTYPE_t,ndim=1] x):
-
-    """ Evaluate generalized Vandermonde matrix of monic polynomials 
+            np.ndarray[DTYPE_t,ndim=1] x,
+            int d=0):
+    """ Evaluate dth derivative of the monic polynomials 
         constructed from the three-term recursion relation
-
+       
         p_{k+1}(x) = (x-alpha_k)p_k(x) - beta_k p_{k-1}(x)
         
         where p_0(x) = 1 and p_1(x) = x-alpha_k
@@ -25,30 +27,25 @@ def polyvander(np.ndarray[DTYPE_t,ndim=1] alpha,
         alpha - array of recursion coefficients
         beta - array of recursion coefficients
         x - array of grid points on which to evaluate the polynomials
+        d - the order of the derivative to compute
     """
-
     if alpha.shape[0] != beta.shape[0]:
         raise ValueError('Unequal length input arrays')    
 
     cdef int N = alpha.shape[0]
     cdef int m = x.shape[0]
     cdef np.ndarray[DTYPE_t,ndim=2] P
- 
-    P = np.zeros((m,N+1),DTYPE)    
-
-    P[:,0] = 1
-    P[:,1] = x-alpha[0]
-
-    for k in range(1,N):
-        P[:,k+1] = (x-alpha[k])*P[:,k] - beta[k]*P[:,k-1]
-
-    return P
-
-
-
-    
+    cdef np.ndarray[DTYPE_t,ndim=2] Px
    
-
-
-
+    if d == 0:
+        P = polyvander(alpha,beta,x)
+        Px = P
+    else:
+        Px = np.zeros((m,N+1),DTYPE)
+        P = polyder(alpha,beta,x,d-1)         
+        Px[:,d] = factorial(d)
+        for k in range(d,N):
+            Px[:,k+1] = d*P[:,k] + (x-alpha[k])*Px[:,k] - beta[k]*Px[:,k-1]
+    return Px
+    
 
